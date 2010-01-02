37e040f6b3c93f8f6b459814a732a5eff81f33b5
Douglas Creager <douglas.creager@redjack.com>
2010-01-01 22:31:05 -0500

    Updating copyright notices to include 2010
    Happy New Year!

--- tests/test-double-sum-callback.c
+++ tests/test-double-sum-callback.c
@@ -1,6 +1,6 @@
 /* -*- coding: utf-8 -*-
  * ----------------------------------------------------------------------
- * Copyright © 2009, RedJack, LLC.
+ * Copyright © 2009-2010, RedJack, LLC.
  * All rights reserved.
  *
  * Please see the LICENSE.txt file in this distribution for license
